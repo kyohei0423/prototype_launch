3ab55b7d9846e0e33b536ccc8a532c1b5982164b
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-06 10:58:30 +0900

    Set up email to send confirmation mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,17 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   # Define default url options
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "uovo.aureo.000@gmail.com",
+    :password => "jk26!!6589",
+    :authentication => 'login',
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
