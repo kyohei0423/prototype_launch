0abde60e5d9fffd43a4a42f942ed1cfd21c1a8bc
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-11 17:31:09 +0900

    add Time.zone.now to confirmed_at column of user with factory girl

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     email                 'aaa@gmail.com'
     password              '00000000'
     password_confirmation '00000000'
+    confirmed_at          Time.zone.now
   end
 end
