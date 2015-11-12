5e1330aaefc799bf5dcf373508f5e6e5d5a17b42
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-12 19:49:52 +0900

    define question_id of factory girl answer instance as faker number

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :answer do
-    sentence    { Faker::ipster.paragraph }
-    user_id     { Faker::Number.number }
+    sentence        { Faker::ipster.paragraph }
+    user_id         { Faker::Number.number }
+    question_id     { Faker::Number.number }
   end
 end
