6e2d3f4252e0b77889a3514b3c62b5b59bda0560
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-12 14:08:57 +0900

    user_idを外部キー化

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111173322) do
+ActiveRecord::Schema.define(version: 20151111173954) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "sentence",    limit: 65535
@@ -59,4 +59,5 @@ ActiveRecord::Schema.define(version: 20151111173322) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
+  add_foreign_key "questions", "users"
 end
