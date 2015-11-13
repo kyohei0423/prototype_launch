4145136be2822b899099a887b911c20d161dd3b9
YuitoSato <yuitosato.w@gmail.com>
2015-11-13 17:01:35 +0900

    Fix the controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!, only: :new
   before_action :set_question, only: :show
-  before_action :ser_user, only: :show
 
   def show
   end
@@ -20,10 +19,6 @@ class QuestionsController < ApplicationController
   end
 
   private
-    def ser_user
-      @user = User.find(params[:id])
-    end
-
     def set_question
       @question = Question.find params[:id]
     end
