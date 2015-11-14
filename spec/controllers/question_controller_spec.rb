cbe63481daea28ac101d995b222c5de0782fee1d
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-15 04:47:26 +0900

    define test about show action of questions controller

--- spec/controllers/question_controller_spec.rb
+++ spec/controllers/question_controller_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 describe QuestionsController do
+  describe 'GET #show' do
+    it 'assigns the requesed constant to @question' do
+      question = create(:question)
+      get :show, id: question
+      expect(assigns(:question)).to eq question
+    end
+  end
+
   describe 'GET #new' do
     it 'render the :new template' do
       get :new
