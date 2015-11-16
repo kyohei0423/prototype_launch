9e569781a36766525a2fb91757d0fe528962e516
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-16 15:09:00 +0900

    define let user in users_controller_spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe UsersController do
   describe 'GET #show' do
+    let(:user) { create(:user) }
+
     it 'assigns the requesed constant to @user' do
-      user = create(:user)
       get :show, id: user
       expect(assigns(:user)).to eq user
     end
 
     it 'render the :show template' do
-      user = create(:user)
       get :show, id: user
       expect(response).to render_template :show
     end
