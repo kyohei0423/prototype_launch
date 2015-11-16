624e5e002a510c76d6d7d4263f499bb0b15eaa68
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-16 15:32:57 +0900

    wrote test code about render :show template

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -24,6 +24,11 @@ describe AnswersController do
       get :show, id: answer, question_id: question
       expect(assigns(:answer)).to eq answer
     end
+
+    it 'render the :show template' do
+      get :show, id: answer, question_id: question
+      expect(response).to render_template(:show)
+    end
   end
 
   describe 'GET #new' do
