7cea3672fa3b55d9da7dd85e7944bac59e053918
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-16 15:16:30 +0900

    write test about before_action :set_user in users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,11 @@ describe UsersController do
   describe 'GET #show' do
     let(:user) { create(:user) }
 
+    it 'execete before_action :set_user' do
+      expect(subject).to receive(:set_user)
+      get :show, id: user
+    end
+
     it 'assigns the requesed constant to @user' do
       get :show, id: user
       expect(assigns(:user)).to eq user
