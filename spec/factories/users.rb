e5010bd18b07b44f0c97760d020671264b9c90a0
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-16 14:11:25 +0900

    edit factory girl about user email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   password = Forgery(:basic).password(at_least: 8)
-  email = Forgery('email').address
+  email = Faker::Internet.email
   factory :user do
-    email                     email
+    sequence(:email)          { |n| "hoge#{n}@gmail.com" }
     password                  password
     password_confirmation     password
     confirmed_at              Time.zone.now
