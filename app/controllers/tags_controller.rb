b59674b5b7c364214d750505552b6aedb8fae8a9
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-18 05:27:49 +0900

    タグの詳細ページへのアクセスをできるようにした

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,16 @@
 class TagsController < ApplicationController
+  before_action :set_tag, only: :show
+
   def index
     @tags = ActsAsTaggableOn::Tag.most_used
   end
+
+  def show
+    @questions = Question.tagged_with(@tag.name)
+  end
+
+  private
+    def set_tag
+      @tag = ActsAsTaggableOn::Tag.find params[:id]
+    end
 end
