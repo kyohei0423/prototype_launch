7d599bd5f4aeb3d5ca91b5014e05f3633c50c42d
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-18 05:28:31 +0900

    タグの詳細ページのルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :questions, only: [:new, :create, :show] do
     resources :answers, only: [:new, :create, :show]
   end
-  resources :tags, only: [:index]
+  resources :tags, only: [:index, :show]
 end
