9534ce31a0dd6fc1a9b5b9f71ec0c76168328fa5
YuitoSato <yuitosato.w@gmail.com>
2015-11-19 03:03:39 +0900

    Fix the index action in the home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+  before_action :set_question, only: :index
+
   def index
   end
+
+  private
+    def set_question
+      @questions = Question.all
+    end
 end
