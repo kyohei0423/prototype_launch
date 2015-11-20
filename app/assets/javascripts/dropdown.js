b3cef5f34c368902a6977ac6009c9cb661886472
YuitoSato <yuitosato.w@gmail.com>
2015-11-21 01:27:01 +0900

    Fix the dropdown menu function and some designs

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,5 +1,12 @@
 window.addEventListener("load", function() {
+  $('.hidden-block').hide();
+
   $('#menuicon').click(function(){
     $("#menu").slideToggle();
-    });
+    $('.hidden-block').show();
+  });
+
+  $('.hidden-block').click(function(){
+    $("#menu").slideUp();
+  });
 });
