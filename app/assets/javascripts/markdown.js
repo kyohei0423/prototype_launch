1bc27d29dfcc3132f298917aef8a0554cecf3fce
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-24 15:16:46 +0900

    questionとanswerの変数textをそれぞれに変更した

--- app/assets/javascripts/markdown.js
+++ app/assets/javascripts/markdown.js
@@ -1,10 +1,10 @@
 $(window).on('load', function() {
   $('#input-textarea').on('keyup', function() {
-    var text = $('#input-textarea').val();
-    document.getElementById('output-area').innerHTML = marked(text);
+    var question = $('#input-textarea').val();
+    document.getElementById('output-area').innerHTML = marked(question);
   });
   $('#input-textarea-answer').on('keyup', function() {
-    var text = $('#input-textarea-answer').val();
-    document.getElementById('output-area-answer').innerHTML = marked(text);
+    var answer = $('#input-textarea-answer').val();
+    document.getElementById('output-area-answer').innerHTML = marked(answer);
   });
 });
