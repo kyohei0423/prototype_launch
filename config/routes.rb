9f07eaefe1876247d7edaf28cf08dbf367ae2647
道祖克理 <uovo.aureo.000@gmail.com>
2015-11-25 14:20:04 +0900

    created api/questions_controller that format is json

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
+
   resources :users, only: [:show]
+
   resources :questions, only: [:new, :create, :show] do
     resources :answers, only: [:new, :create, :show]
   end
+
   resources :tags, only: [:index, :show]
+
+  namespace :api, defaults: { format: :json } do
+    resources :questions, only: :index do
+      collection do
+        get 'newest'
+        get 'popular'
+        get 'easier'
+      end
+    end
+  end
+
+  root 'home#index'
 end
