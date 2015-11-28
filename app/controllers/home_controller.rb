17228d742a3aeaaf4a45b001582d6366c805ef9d
YuitoSato <yuitosato.w@gmail.com>
2015-11-28 14:16:20 +0900

    Define search action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,10 @@ class HomeController < ApplicationController
   def index
   end
 
+  def search
+    @questions = Book.where('title LIKE(?)',"%#{search_params[:keyword]}%").order('title ASC').limit(20)
+  end
+
   private
     def set_questions
       @questions = Question.all
