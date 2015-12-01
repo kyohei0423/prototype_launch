dce30f141ef046746fc8e6f5a59b9ac2cae63e98
道祖克理 <uovo.aureo.000@gmail.com>
2015-12-01 18:42:02 +0900

    サイドバーのランキングを作成した

---
+++ app/assets/javascripts/side-bar.js
@@ -0,0 +1,10 @@
+$(window).on('load', function() {
+  $('.nav-tabs li').on('click', function() {
+    var newTab = $(this).attr('id'),
+        oldTab = $('.active').attr('id');
+    $('li').removeClass('active');
+    $(this).addClass('active');
+    $('.elements').hide();
+    $('.' + newTab).show();
+  });
+});
