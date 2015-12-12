8ee836df1e79f2184fb45c383a6469fb7edd857f
道祖克理 <uovo.aureo.000@gmail.com>
2015-12-12 18:52:24 +0900

    add avatar to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212093410) do
+ActiveRecord::Schema.define(version: 20151212095019) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "sentence",    limit: 65535
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20151212093410) do
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
+    t.string   "nickname",               limit: 255
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
@@ -85,7 +86,7 @@ ActiveRecord::Schema.define(version: 20151212093410) do
     t.string   "unconfirmed_email",      limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
-    t.string   "nickname",               limit: 255
+    t.string   "avatar",                 limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
