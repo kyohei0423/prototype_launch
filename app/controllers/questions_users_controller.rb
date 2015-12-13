289d6ba731048958a93705c9e5018cbf09aeafb6
YuitoSato <yuitosato.w@gmail.com>
2015-12-13 21:16:03 +0900

    Fix the questions users controller

--- app/controllers/questions_users_controller.rb
+++ app/controllers/questions_users_controller.rb
@@ -1,5 +1,7 @@
 class QuestionsUsersController < ApplicationController
   def create
-    QuestionsUser.create(user_id: current_user.id, question_id: params[:question_id], status: 0)
+    QuestionsUser.create(user_id: current_user.id, question_id: params[:question_id], status: params[:status].to_i)
+
+    @question = Question.find(params[:question_id])
   end
 end
