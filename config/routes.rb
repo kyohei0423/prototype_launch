85cfedc959b5f217155ed8af08af78b5e2d9b86d
道祖克理 <uovo.aureo.000@gmail.com>
2015-12-14 18:33:37 +0900

    routes.rbのリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
+
   namespace :questions do
     resources :newest, only: :index
   end
+
   resources :questions, only: [:new, :create, :show, :destroy, :edit, :update] do
     resources :answers, only: [:new, :create, :show]
   end
+
   resources :tags, only: [:index, :show]
+
   get 'search' => 'home#search'
+
   root 'home#index'
 end
