4603c77055a167d1304395d17057b8ca9833a86d
YuitoSato <yuitosato.w@gmail.com>
2015-12-15 00:26:01 +0900

    Delete and add lines in some files

--- app/controllers/questions_users_controller.rb
+++ app/controllers/questions_users_controller.rb
@@ -1,7 +1,6 @@
 class QuestionsUsersController < ApplicationController
   def create
-    QuestionsUser.create(user_id: current_user.id, question_id: params[:question_id], status: params[:status].to_i)
-
+    QuestionsUser.create(user_id: current_user.id, question_id: params[:question_id], status: params[:status])
     @question = Question.find(params[:question_id])
   end
 end
--- app/models/questions_user.rb
+++ app/models/questions_user.rb
@@ -1,5 +1,6 @@
 class QuestionsUser < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
-  enum status: %i(answered, unanswered)
+
+  enum status: %i(answered unanswered)
 end
