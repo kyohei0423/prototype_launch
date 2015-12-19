50646b05309b31a6833a910dc3741deda81bb146
YuitoSato <yuitosato.w@gmail.com>
2015-12-19 16:55:10 +0900

    Define the method to update a user's level

--- app/controllers/questions_users_controller.rb
+++ app/controllers/questions_users_controller.rb
@@ -5,11 +5,9 @@ class QuestionsUsersController < ApplicationController
     current_user.questions_users.create(questions_user_params)
     @question = Question.find(params[:question_id])
     @question_level = 7
-    exp = @question
-
-    current_user.exp += exp
-    current_user.exp = (10(current_ues.exp-3))**(1/2.0)
-    binding.pry
+    if params[:status] == "answered"
+      current_user.update_level(@question)
+    end
   end
 
   private
