e5e5274e0415ee7ec9978a175c86d36ea5e7bd89
YuitoSato <yuitosato.w@gmail.com>
2015-12-19 21:56:12 +0900

    Define create and destroy actions in the comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :set_question, only: [:create, :destroy]
+
+  def create
+    @comment = current_user.comments.create(comment_params)
+  end
+
+  def destroy
+    comment = Comment.find params[:id]
+    if comment.user.id == current_user.id
+      comment.destroy
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(question_id: params[:question_id])
+  end
+
+  def set_question
+    @question = Question.includes(:questions_users, :keeps, :comments).find params[:question_id]
+  end
 end
