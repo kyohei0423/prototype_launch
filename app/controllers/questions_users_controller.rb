c326f7d151aadcf7c5ec01bb7cf68a2260d90fe9
YuitoSato <yuitosato.w@gmail.com>
2015-12-19 15:57:08 +0900

    Define the equation to calculate a user's level

--- app/controllers/questions_users_controller.rb
+++ app/controllers/questions_users_controller.rb
@@ -4,6 +4,12 @@ class QuestionsUsersController < ApplicationController
   def create
     current_user.questions_users.create(questions_user_params)
     @question = Question.find(params[:question_id])
+    @question_level = 7
+    exp = @question
+
+    current_user.exp += exp
+    current_user.exp = (10(current_ues.exp-3))**(1/2.0)
+    binding.pry
   end
 
   private
