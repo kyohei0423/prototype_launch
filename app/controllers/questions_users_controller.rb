d29f1adc4dce6d42139e3b8b18ccf80974306081
道祖克理 <uovo.aureo.000@gmail.com>
2015-12-25 11:45:59 +0900

    not completeが動作しない不具合を解決した

--- app/controllers/questions_users_controller.rb
+++ app/controllers/questions_users_controller.rb
@@ -1,16 +1,21 @@
 class QuestionsUsersController < ApplicationController
   before_action :authenticate_user!, only: :create
+  before_action :set_question, only: :create
 
   def create
     current_user.questions_users.create(questions_user_params)
     if params[:status] == "answered"
-      @question = Question.find(params[:question_id])
       current_user.update_level(@question)
     end
   end
 
   private
+
     def questions_user_params
       params.permit(:question_id, :status)
+    end
+
+    def set_question
+      @question = Question.find params[:question_id]
     end
 end
